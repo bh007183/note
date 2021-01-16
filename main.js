5c93e10a5318ae5d10b553142fb80951ece589a5
Ben Hopkins <bjhops17@gmail.com>
2021-01-16 10:17:30 -0800

    delete buttons functional

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 
 var app = express();
-var PORT = 8080;
+var PORT = 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -54,22 +54,26 @@ app.post("/api/notes", function(request, response){
 })
 
 app.delete("/api/notes/:id", function(request, response){
-    const selectParams = request.params.id
-    console.log(selectParams)
+    const selectParams = request.params.id //////this refurs to the path of id in the url so if the trashcan associated with this it grabs the number
     fs.readFile(path.join(__dirname, "../note/db/db.json"), "utf8", function(error, data){
       if(error){return response.sendStatus(404)} 
       let mainData = JSON.parse(data)
-      // for (let i = 0; i < mainData.length; i++) {
-      if (selectParams === mainData.indexOf(mainData.id)){
-        console.log(selectParams)
-        console.log(mainData[i])
-          mainData.splice(i, 1)
-      }
-    // }
-        fs.writeFile(path.join(__dirname, "../note/db/db.json"), JSON.stringify(mainData), function(err){
+      console.log(mainData)
+      console.log(mainData[0].id)
+      for(var i = 0; i < mainData.length; i++){
+        if((mainData[i].id).toString() === selectParams){
+             mainData.splice(mainData.indexOf(mainData[i]), 1)
+            }
+          }
+         
+        
+        
+         
+         fs.writeFile(path.join(__dirname, "../note/db/db.json"), JSON.stringify(mainData), function(err){
           if(err) throw err
           response.json(request.body)
 })
+        
 })
 })
 app.listen(PORT, function() {
