2718052791aa98cba5c85ad9040f34637d252782
Ben Hopkins <bjhops17@gmail.com>
2021-01-19 09:12:29 -0800

    test4 env variable

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 
 
 var app = express();
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
