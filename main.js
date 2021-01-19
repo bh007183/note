2bfca5553cb3ed8ac011cc7c9b4bdaf3573e7870
Ben Hopkins <bjhops17@gmail.com>
2021-01-19 09:37:15 -0800

    test7

--- main.js
+++ main.js
@@ -12,16 +12,16 @@ app.use(express.static("public"))
 
 
 app.get("/notes", function(request, response) {
-    response.sendFile(path.join(__dirname, "../note/public/notes.html"));
+    response.sendFile(path.join(__dirname, "./public/notes.html"));
   });
 app.get("/", function(request, response) {
-    response.sendFile(path.join(__dirname, "../note/public/index.html"));
+    response.sendFile(path.join(__dirname, "./public/index.html"));
   });
 
 //////////////////////////////////////////////////////////////////////////
 
 app.get("/api/notes", function(request, response){
-  fs.readFile(path.join(__dirname, "../note/db/db.json"), "utf8", function(err, data){
+  fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", function(err, data){
     if(err){
         console.log("error")
     }
@@ -33,7 +33,7 @@ app.get("/api/notes", function(request, response){
 
 app.post("/api/notes", function(request, response){
     
-     fs.readFile(path.join(__dirname, "../note/db/db.json"), "utf8", function(error, data){
+     fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", function(error, data){
       if(error){return response.sendStatus(404)} 
       let mainData = JSON.parse(data)
       mainData.push(request.body)
@@ -41,7 +41,7 @@ app.post("/api/notes", function(request, response){
       for(let i = 0; i < mainData.length; i++){
         mainData[i].id = i  
       }
-        fs.writeFile(path.join(__dirname, "../note/db/db.json"), JSON.stringify(mainData), function(err){
+        fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(mainData), function(err){
           if(err) throw err
           response.json(request.body)
 })
@@ -50,7 +50,7 @@ app.post("/api/notes", function(request, response){
 
 app.delete("/api/notes/:id", function(request, response){
     const selectParams = request.params.id 
-    fs.readFile(path.join(__dirname, "../note/db/db.json"), "utf8", function(error, data){
+    fs.readFile(path.join(__dirname, "./db/db.json"), "utf8", function(error, data){
       if(error){return response.sendStatus(404)} 
       let mainData = JSON.parse(data)
       for(var i = 0; i < mainData.length; i++){
@@ -58,7 +58,7 @@ app.delete("/api/notes/:id", function(request, response){
              mainData.splice(mainData.indexOf(mainData[i]), 1)
             }
           }
-         fs.writeFile(path.join(__dirname, "../note/db/db.json"), JSON.stringify(mainData), function(err){
+         fs.writeFile(path.join(__dirname, "./db/db.json"), JSON.stringify(mainData), function(err){
           if(err) throw err
           response.json(request.body)
 })
